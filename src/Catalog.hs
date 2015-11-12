2a8ade74245dfe4f786fbaf7502be6ffc8e33e64
Harold Carr <harold.carr@gmail.com>
2015-11-11 19:36:08 -0800

    Use a local utility.

--- src/Catalog.hs
+++ src/Catalog.hs
@@ -1,6 +1,6 @@
 {-
 Created       : 2014 Jul 24 (Thu) 09:37:09 by Harold Carr.
-Last Modified : 2015 Nov 11 (Wed) 18:18:01 by Harold Carr.
+Last Modified : 2015 Nov 11 (Wed) 19:33:19 by Harold Carr.
 -}
 
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -8,27 +8,15 @@ Last Modified : 2015 Nov 11 (Wed) 18:18:01 by Harold Carr.
 
 module Main where
 
-import           Control.Monad                          (forM_)
-import           Data.GraphViz
-import           Data.GraphViz.Attributes.Colors.Brewer
-import           Data.GraphViz.Attributes.Complete
--- import           Data.GraphViz.HC.Util
-import           Data.GraphViz.Types.Generalised        as G
-import           Data.GraphViz.Types.Monadic
-import           Data.Text.Lazy                         as L
-import           Data.Word                              (Word8 (..))
-import           System.FilePath
+import           Data.GraphViz                     (GraphID (Str), GraphvizCommand (Fdp, Fdp, TwoPi))
+import           Data.GraphViz.Attributes.Complete (Attribute (RankDir),
+                                                    RankDir (FromLeft))
+import           Data.GraphViz.HC.Util             (doDots', uRectangle)
+import           Data.GraphViz.Types.Generalised   as G (DotGraph)
+import           Data.GraphViz.Types.Monadic       (digraph, graphAttrs, (-->))
+import           Data.Text.Lazy                    as L (Text)
 
 ------------------------------------------------------------------------------
-
-uBaseShape             :: [Attribute] -> n -> Text -> Dot n
-uBaseShape      as n l = node n $ [textLabel l, style filled] ++ as
-
-pastel28 :: Word8 -> Attribute
-pastel28 n = Color (toColorList [toColor (BC (BScheme Pastel2 8) n)])
-
-uRectangle             :: [Attribute] -> n -> Text -> Dot n
-uRectangle      as     = uBaseShape $ [shape     BoxShape,   pastel28 5] ++ as
 
 f = uRectangle     []
 
@@ -79,18 +67,6 @@ queryableStore = digraph (Str "queryableStore") $ do
     "qsGetSCategories"  --> "store"
 
 ------------------------------------------------------------------------------
-
-doDots :: PrintDotRepr dg n => [(FilePath, dg n)] -> IO ()
-doDots cases = doDots' Dot cases
-
-doDots' :: PrintDotRepr dg n => GraphvizCommand -> [(FilePath, dg n)] -> IO ()
-doDots' command cases = forM_ cases (createImage command)
-
-createImage :: PrintDotRepr dg n => GraphvizCommand -> (FilePath, dg n) -> IO FilePath
-createImage command (n, g) = createImageInDir command "/tmp" n Png g
-
-createImageInDir :: PrintDotRepr dg n => GraphvizCommand -> FilePath -> FilePath -> GraphvizOutput -> dg n -> IO FilePath
-createImageInDir c d n o g = Data.GraphViz.addExtension (runGraphvizCommand c g) o (combine d n)
 
 main :: IO ()
 main =
