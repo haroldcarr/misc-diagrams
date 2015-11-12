32ad5fd84b14b753696f9bb0e082ae12ad1fef97
Harold Carr <harold.carr@gmail.com>
2015-11-11 20:33:57 -0800

    Consolidating more diagrams.

--- src/Catalog.hs
+++ src/Catalog.hs
@@ -1,6 +1,6 @@
 {-
 Created       : 2014 Jul 24 (Thu) 09:37:09 by Harold Carr.
-Last Modified : 2015 Nov 11 (Wed) 19:43:39 by Harold Carr.
+Last Modified : 2015 Nov 11 (Wed) 20:19:24 by Harold Carr.
 -}
 
 {-# LANGUAGE MultiParamTypeClasses #-}
